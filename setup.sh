6a170c3ed09634fb293cfec9d90c5f30020b9c18
MrPatcher <yassine.eljakani@edu.uca.ma>
2024-05-27 00:36:55 +0100

    update

--- setup.sh
+++ setup.sh
@@ -206,7 +206,7 @@ main() {
                 suricata_network_setup
                 install_latest_filebeat
                 interactive_setup_filebeat
-                install_kafka
+                #install_kafka
                 ;;
             *)
                 echo "Invalid choice"
