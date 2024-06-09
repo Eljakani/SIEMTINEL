bfe9d1f664ca7359d5818fbc269d8de30fb7a743
MrPatcher <yassine.eljakani@edu.uca.ma>
2024-06-09 22:53:33 +0100

    beautify the output

--- setup.sh
+++ setup.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+# Function to display project name using figlet
+show_project_name() {
+    clear
+    # check if figlet is installed
+    if ! [ -x "$(command -v figlet)" ]; then
+        sudo apt install figlet -y >/dev/null 2>&1
+    fi
+    figlet "SIEMTINEL" -f slant
+    echo -e "\e[34mSIEMTINEL - A Cloud-Based SIEM\e[0m"
+}
+show_whenfinished() {
+    echo -e "\e[34m[+] The setup is complete. Please visit the controller at http://$CONTROLLER_IP:5601\e[0m"
+}
+
+# Function to show a spinner while a command is running
+spinner() {
+    local pid=$!
+    local delay=0.1
+    local spinstr='|/-\'
+    tput civis
+    while [ "$(ps a | awk '{print $1}' | grep "$pid")" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+    tput cnorm
+}
+
 # Function to detect the Linux distribution
 detect_distro() {
     if [ -f /etc/os-release ]; then
@@ -17,48 +48,45 @@ detect_distro() {
     fi
     echo $DISTRO
 }
+
 # Function to install suricata and copy the suricata.yml file
 install_suricata() {
     DISTRO=$(detect_distro)
-    sudo apt install wget curl nano software-properties-common dirmngr apt-transport-https gnupg gnupg2 ca-certificates lsb-release ubuntu-keyring unzip -y
-    sudo add-apt-repository ppa:oisf/suricata-stable -y
-    sudo apt-get update
-    sudo apt-get install suricata -y
-    sudo systemctl enable suricata
-    sudo systemctl stop suricata
-    # community-id: true in /etc/suricata/suricata.yaml
+    echo -e "\e[34m[+] Installing prerequisites...\e[0m"
+    sudo apt install wget curl nano software-properties-common dirmngr apt-transport-https gnupg gnupg2 ca-certificates lsb-release ubuntu-keyring unzip -y >/dev/null 2>&1 & spinner
+    echo -e "\e[34m[+] Adding Suricata PPA...\e[0m"
+    sudo add-apt-repository ppa:oisf/suricata-stable -y >/dev/null 2>&1 & spinner
+    echo -e "\e[34m[+] Updating package list...\e[0m"
+    sudo apt-get update >/dev/null 2>&1 & spinner
+    echo -e "\e[34m[+] Installing Suricata...\e[0m"
+    sudo apt-get install suricata -y >/dev/null 2>&1 & spinner
+    sudo systemctl enable suricata >/dev/null 2>&1
+    sudo systemctl stop suricata >/dev/null 2>&1
     sudo sed -i 's/# community-id: false/community-id: true/g' /etc/suricata/suricata.yaml
-    # find the line pcap: and under it, set the value of the variable interface to the device name for your system
     sudo sed -i 's/# pcap:/pcap:/g' /etc/suricata/suricata.yaml
-    #replace the eth0 with the interface variable chosen by the user in the sensor_setup_info() function
     sudo sed -i "s/interface: eth0/interface: $interface/g" /etc/suricata/suricata.yaml
-    # #use-mmap: yes
     sudo sed -i 's/# use-mmap: yes/use-mmap: yes/g' /etc/suricata/suricata.yaml
-    # enable capture-settings
-    sudo suricata-update
-    sudo suricata-update list-sources
-    #TODO add the wazuuh rules
-    sudo suricata-update enable-source tgreen/hunting
-    sudo suricata -T -c /etc/suricata/suricata.yaml -v
-    sudo systemctl start suricata
+    sudo suricata-update >/dev/null 2>&1
+    sudo suricata-update list-sources >/dev/null 2>&1
+    echo -e "\e[34m[+] Enabling Wazuh rules...\e[0m"
+    sudo suricata-update enable-source tgreen/hunting >/dev/null 2>&1
+    sudo suricata -T -c /etc/suricata/suricata.yaml -v >/dev/null 2>&1
+    sudo systemctl start suricata >/dev/null 2>&1
 }
 
-
-suricata_network_setup(){
-    # interface configuration
-    sudo ip link set $interface multicast off
-    sudo ip link set $interface promisc on
-    sudo ip link set $interface up
+suricata_network_setup() {
+    sudo ip link set $interface multicast off >/dev/null 2>&1
+    sudo ip link set $interface promisc on >/dev/null 2>&1
+    sudo ip link set $interface up >/dev/null 2>&1
 }
+
 is_valid_interface() {
     local interface="$1"
     ip link show "$interface" >/dev/null 2>&1
 }
 
-sensor_setup_info(){
-    # using whiptail to list all intefaces and make the user choose one to use as sniffer 
+sensor_setup_info() {
     interfaces=$(ip link show | awk -F': ' '/state UP/ {print $2}')
-    # choose an interface to use as sniffer
     echo "Available network interfaces:"
     select interface in $interfaces; do
         if is_valid_interface "$interface"; then
@@ -69,46 +97,43 @@ sensor_setup_info(){
         fi
     done
 
-    # If a valid interface is chosen, proceed with the script
     if [ -n "$interface" ]; then
-        # Your script logic here
         echo "Continuing with interface: $interface"
     else
         echo "No valid interface selected. Exiting."
         exit 1
     fi
-    # ask for the IP address of the controller
+
     CONTROLLER_IP=$(whiptail --inputbox "Enter the IP address of the controller" 8 78 --title "Controller IP" 3>&1 1>&2 2>&3)
     echo "Controller IP: $CONTROLLER_IP"
-    # ask for the username of the controller and the password
     CONTROLLER_USERNAME=$(whiptail --inputbox "Enter the username of the controller" 8 78 --title "Controller Username" 3>&1 1>&2 2>&3)
     echo "Controller Username: $CONTROLLER_USERNAME"
     CONTROLLER_PASSWORD=$(whiptail --passwordbox "Enter the password of the controller" 8 78 --title "Controller Password" 3>&1 1>&2 2>&3)
-    # password as **
     echo "Controller Password: **"
 }
 
-
-# Function to install Docker
 install_docker() {
     DISTRO=$(detect_distro)
     case "$DISTRO" in
         "ubuntu" | "debian")
-            sudo apt-get update
-            sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-            sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-            sudo apt-get update
-            sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+            echo -e "\e[34m[+] Installing Docker...\e[0m"
+            sudo apt-get update >/dev/null 2>&1 & spinner
+            sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common >/dev/null 2>&1 & spinner
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >/dev/null 2>&1 & spinner
+            sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y >/dev/null 2>&1 & spinner
+            sudo apt-get update >/dev/null 2>&1 & spinner
+            sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose >/dev/null 2>&1 & spinner
             ;;
         "centos" | "rhel")
-            sudo yum install -y yum-utils
-            sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-            sudo yum install -y docker-ce docker-ce-cli containerd.io
+            echo -e "\e[34m[+] Installing Docker...\e[0m"
+            sudo yum install -y yum-utils >/dev/null 2>&1 & spinner
+            sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo >/dev/null 2>&1 & spinner
+            sudo yum install -y docker-ce docker-ce-cli containerd.io >/dev/null 2>&1 & spinner
             ;;
         "fedora")
-            sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-            sudo dnf install -y docker-ce docker-ce-cli containerd.io
+            echo -e "\e[34m[+] Installing Docker...\e[0m"
+            sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo >/dev/null 2>&1 & spinner
+            sudo dnf install -y docker-ce docker-ce-cli containerd.io >/dev/null 2>&1 & spinner
             ;;
         *)
             echo "Unsupported distribution: $DISTRO"
@@ -118,21 +143,20 @@ install_docker() {
 }
 
 install_latest_filebeat() {
-    # get the version of filebeat from .env file 
     DISTRO=$(detect_distro)
     VERSION=$(grep ELASTIC_VERSION .env | cut -d '=' -f2)
     case "$DISTRO" in
         "ubuntu" | "debian")
-            curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-$VERSION-amd64.deb
-            sudo dpkg -i filebeat-$VERSION-amd64.deb
+            echo -e "\e[34m[+] Downloading Filebeat...\e[0m"
+            curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-$VERSION-amd64.deb >/dev/null 2>&1 & spinner
+            echo -e "\e[34m[+] Installing Filebeat...\e[0m"
+            sudo dpkg -i filebeat-$VERSION-amd64.deb >/dev/null 2>&1 & spinner
             ;;
-        "centos" | "rhel")
-            curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-$VERSION-x86_64.rpm
-            sudo rpm -vi filebeat-$VERSION-x86_64.rpm
-            ;;
-        "fedora")
-            curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-$VERSION-x86_64.rpm
-            sudo rpm -vi filebeat-$VERSION-x86_64.rpm
+        "centos" | "rhel" | "fedora")
+            echo -e "\e[34m[+] Downloading Filebeat...\e[0m"
+            curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-$VERSION-x86_64.rpm >/dev/null 2>&1 & spinner
+            echo -e "\e[34m[+] Installing Filebeat...\e[0m"
+            sudo rpm -vi filebeat-$VERSION-x86_64.rpm >/dev/null 2>&1 & spinner
             ;;
         *)
             echo "Unsupported distribution: $DISTRO"
@@ -143,52 +167,44 @@ install_latest_filebeat() {
 
 interactive_setup_filebeat() {
     sudo cp filebeat/filebeat.yml /etc/filebeat/filebeat.yml
-
     sudo sed -i "s/CONTROLLER_IP/$CONTROLLER_IP/g" /etc/filebeat/filebeat.yml
-    # replace CONTROLLER_USERNAME in filebeat/filebeat.yml with the actual username
     sudo sed -i "s/CONTROLLER_USERNAME/$CONTROLLER_USERNAME/g" /etc/filebeat/filebeat.yml
-    # replace CONTROLLER_PASSWORD in filebeat/filebeat.yml with the actual password
     sudo sed -i "s/CONTROLLER_PASSWORD/$CONTROLLER_PASSWORD/g" /etc/filebeat/filebeat.yml
-    # add /var/log/suricata/eve.json to the paths in filebeat/filebeat.yml
     sudo sed -i 's/# paths:/paths:/g' /etc/filebeat/filebeat.yml
     sudo sed -i 's/#   - \/var\/log\/*.log/   - \/var\/log\/suricata\/eve.json/g' /etc/filebeat/filebeat.yml
-    # enable and start the filebeat service
-    sudo systemctl enable filebeat
-    # enable the suricata module
-    sudo filebeat modules enable suricata
-    # setup the suricata module
-    sudo filebeat setup
-    # start the filebeat service
-    sudo systemctl start filebeat
-
+    sudo systemctl enable filebeat >/dev/null 2>&1
+    sudo filebeat modules enable suricata >/dev/null 2>&1
+    sudo filebeat setup >/dev/null 2>&1
+    sudo systemctl start filebeat >/dev/null 2>&1
 }
 
 start_project() {
-    sudo docker compose up setup
-    sudo docker compose up -d
+    sudo docker compose up setup >/dev/null 2>&1 & spinner
+    sudo docker compose up -d >/dev/null 2>&1 & spinner
 }
 
 main() {
+    show_project_name
     choice=$(whiptail --title "Machine Type" --menu "Is this machine a controller or a sensor?" 15 60 2 \
         "1" "Controller" \
         "2" "Sensor" \
         3>&1 1>&2 2>&3)
-        case $choice in
-            1)
-                install_docker
-                start_project
-                ;;
-            2)
-                sensor_setup_info
-                install_suricata
-                suricata_network_setup
-                install_latest_filebeat
-                interactive_setup_filebeat
-                ;;
-            *)
-                echo "Invalid choice"
-                ;;
-        esac
+    case $choice in
+        1)
+            install_docker
+            start_project
+            ;;
+        2)
+            sensor_setup_info
+            install_suricata
+            suricata_network_setup
+            install_latest_filebeat
+            interactive_setup_filebeat
+            ;;
+        *)
+            echo "Invalid choice"
+            ;;
+    esac
 }
 
 main
